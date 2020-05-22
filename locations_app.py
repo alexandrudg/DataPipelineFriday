6d138990001b010d7e620f6535be2423bfb6f2a9
Alexandru Dina-Gargala <dgalexandru@Alexs-MacBook.local>
2020-05-22 11:39:23 +0200

    Added update server path

--- locations_app.py
+++ locations_app.py
@@ -1,8 +1,8 @@
 # http://sonalinayak.pythonanywhere.com/yelp/api/v1.0/category/Kabuki
 # A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, request
 import sqlite3
+import git
 
 app = Flask(__name__)
 
@@ -25,3 +25,13 @@ def get_categories(restaurant):
     if len(row) == 0:
         abort(404)
     return jsonify({'categories': categories[0]})
+
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('https://github.com/alexandrudg/DataPipelineFriday')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
