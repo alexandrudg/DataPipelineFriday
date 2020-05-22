4978b9155343e4488c7dd46136c13122f3f81423
Alexandru Dina-Gargala <dgalexandru@Alexs-MacBook.local>
2020-05-22 10:49:19 +0200

    Small cleanup of env files

--- restaurants-loader.py
+++ restaurants-loader.py
@@ -5,6 +5,7 @@ import sqlite3
 
 # import general settings
 config = yaml.safe_load(open("config.yaml"))
+environment = yaml.safe_load(open("environment.yaml"))
 locations = config['restaurant_locations']
 
 for location in locations:
@@ -12,21 +13,18 @@ for location in locations:
   offset = 0
   while offset <= maximum:
     # Request restaurants and encoding
-    url = config['API-ROOT'] + "location=" + location["query"] + "&categories=" + location["categories"] + "&limit=50" + "&offset=" + str(offset)
-    print(url)
-    headers = {'Authorization': 'Bearer ' + config['API-KEY'] }
+    url = environment['API-ROOT'] + "location=" + location["query"] + "&categories=" + location["categories"] + "&limit=50" + "&offset=" + str(offset)
+    headers = {'Authorization': 'Bearer ' + environment['API-KEY'] }
     r = requests.get(url=url, headers=headers)
     text = r.text
     parsed = json.loads(text)
-    print(parsed)
     restaurants = parsed['businesses']
     maximum = min(parsed['total'], 950)
-    print(offset)
     offset += 50
-    print(maximum)
 
     formatted_restaurants = []
     for restaurant in restaurants:
+      #Preparing the JSON entries for sqlite
       formatted_restaurant = {
         'id': restaurant['id'],
         'name': restaurant['name'],
@@ -35,13 +33,12 @@ for location in locations:
         'categories': ', '.join(d['title'] for d in restaurant['categories']),
         'rating': restaurant['rating'],
         'price': restaurant['price'] if 'price' in restaurant else '',
-        'street': restaurant['location']['address1'] ,
+        'street': restaurant['location']['address1'],
         'city': restaurant['location']['city'],
         'zip_code': restaurant['location']['zip_code'],
         'state': restaurant['location']['state']
       }
       formatted_restaurants.append(formatted_restaurant)
-
 
     # Connect to sqlite & prepare table if it's not there
     qry = config['TABLE-CREATE'].format(table=location['table'])
@@ -49,7 +46,7 @@ for location in locations:
     c = con.cursor()
     c.execute(qry)
 
-    # Create secondary index
+    # Create secondary index if it does not exist
     createSecondaryIndex = "CREATE INDEX IF NOT EXISTS " + location["secondary_index"] + " ON " + location[
       "table"] + "(" + location["secondary_index"] + ")"
     c.execute(createSecondaryIndex)
