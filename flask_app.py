a0128313b3e146ac1cfc26b0b70af32b45403746
Alexandru Dina-Gargala <dgalexandru@Alexs-MacBook.local>
2020-05-22 12:32:14 +0200

    Fix git path for pull

--- flask_app.py
+++ flask_app.py
@@ -29,7 +29,7 @@ def get_categories(restaurant):
 @app.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('/DataPipelineFriday')
+        repo = git.Repo('/home/alexandrudg/DataPipelineFriday')
         origin = repo.remotes.origin
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
